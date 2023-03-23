43885509e32e1610b2dbb6d372e7b7d7736690d7
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-03-23 14:28:45 +0200

    init

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -30,18 +30,18 @@ class Feedback extends Component {
         return (
             <div className={css.feedback__card}>
                 <h1 className={css.feedback__title}>Please leave feedback</h1>
-                <div className={css.feedback__cardButton}>
-                    <button className={css.feedback__button} onClick={this.handleClickIncrementGood}>Good</button>
-                    <button className={css.feedback__button} onClick={this.handleClickIncrementNeutral}>Neutral</button>
-                    <button className={css.feedback__button} onClick={this.handleClickIncrementBad}>Bad</button>
+                <div className={css.card__button}>
+                    <button className={css.button} onClick={this.handleClickIncrementGood}>Good</button>
+                    <button className={css.button} onClick={this.handleClickIncrementNeutral}>Neutral</button>
+                    <button className={css.button} onClick={this.handleClickIncrementBad}>Bad</button>
                 </div>
                 <h2 className={css.feedback__title}>Statistics</h2>
-                <div className={css.feedback__statistics}>
-                    <p>Good: {this.state.good}</p>
-                    <p>Neutral: {this.state.neutral}</p>
-                    <p>Bad: {this.state.bad}</p>
-                    <p>Total: </p>
-                    <p>Positive feedback: </p>
+                <div className={css.statistics}>
+                    <p className={css.statistics__text}>Good: {this.state.good}</p>
+                    <p className={css.statistics__text}>Neutral: {this.state.neutral}</p>
+                    <p className={css.statistics__text}>Bad: {this.state.bad}</p>
+                    <p className={css.statistics__text}>Total: </p>
+                    <p className={css.statistics__text}>Positive feedback: </p>
                 </div>
             </div>
         )
